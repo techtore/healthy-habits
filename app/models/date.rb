b40c94b741b55843da22e390c3c93528290943e3
Victoria Hale <vic.hale74@gmail.com>
2019-12-02 13:46:58 -0800

    added associations and date model

---
+++ app/models/date.rb
@@ -0,0 +1,3 @@
+class Date < ActiverRecord::Base
+    belongs_to :habit
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+    has_secure_password
     has_many :habits
+    has_many :dates, through: :habits
 end
