bd9478a1b0a13eb9cc836c716e146c112ea0723f
Victoria Hale <vic.hale74@gmail.com>
2019-12-03 07:38:34 -0800

    edit associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_secure_password
-    has_many :habits
-    has_many :dates, through: :habits
+    has_many :dates
+    has_many :habits, through: :dates
 end
