36bfd098be5d81fc70e6a8389446a804c25d4461
Victoria Hale <vic.hale74@gmail.com>
2019-12-04 11:41:06 -0800

    helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret "habit_tracker"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do 
+    def logged_in?
+      !!session[:user_id]
+      #if user exists, true, if not, false
+    end
+
+    def current_user
+      @user ||= User.find_by(id: session[:user_id])
+    end
   end
 
 end
