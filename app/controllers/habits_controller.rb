86c5bd29e7308ecfafa80e1da1a07e9ba09359a6
Victoria Hale <vic.hale74@gmail.com>
2019-12-04 14:51:01 -0800

    new habit creation route

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -9,4 +9,22 @@ class HabitsController < ApplicationController
             redirect "/login"  
         end
     end
+
+    get '/habits/new' do 
+        if logged_in?
+            erb :'habits/new'
+        else
+            redirect '/'
+        end
+    end
+
+    post '/habits' do 
+        user = User.find_by(id: params[:user_id])
+        habit = Habit.create(name: params[:name], date: params[:date], notes: params[:notes])
+        if habit.save
+            redirect '/habits'
+        else
+            redirect '/habits/new'
+        end
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,9 @@ class SessionsController < ApplicationController
             redirect "/login"
         end
     end
+
+    get "/logout" do 
+        session.clear
+        redirect "/"
+    end 
 end
