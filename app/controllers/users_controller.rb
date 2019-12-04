75f02ae822e5496fdc7be1566945c8b993c084ec
Victoria Hale <vic.hale74@gmail.com>
2019-12-04 14:13:13 -0800

    added signup routes and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
 
+    get "/signup" do 
+            erb :"users/signup"
+       
+    end
+
+    post "/signup" do 
+        user = User.new(params)
+        if user.save
+            session["user_id"] = user.id
+            redirect "/habits"
+        else
+            redirect "/signup"
+        end
+    end
 end
