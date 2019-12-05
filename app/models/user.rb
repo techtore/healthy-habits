3b3d1d39393b0cf3df19a880ddacdf6815c7a62d
Victoria Hale <vic.hale74@gmail.com>
2019-12-05 11:51:22 -0800

    added email format to validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     validates :name, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, format: { with: /A[^@s] @[^@s] z/, allow_blank: false }
     validates :email, uniqueness: true
     has_many :habits
 end
