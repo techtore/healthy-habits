ff02c21f6e06055562a8fb59bb981ecb63e6ba73
Victoria Hale <vic.hale74@gmail.com>
2019-12-05 09:57:39 -0800

    added show route and fixed new view to show date

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -21,10 +21,31 @@ class HabitsController < ApplicationController
     post '/habits' do 
         user = User.find_by(id: params[:user_id])
         habit = Habit.create(name: params[:name], date: params[:date], notes: params[:notes], user_id: current_user.id)
-        if habit.save
+        if habit
             redirect '/habits'
         else
             redirect '/habits/new'
         end
+    end
+
+    get '/habits/:id' do
+        if logged_in? && current_user
+            @habit = Habit.find_by(id: params[:id])
+            erb :'habits/show'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/habits/:id/edit' do 
+
+    end
+
+    patch '/habits/:id' do 
+
+    end
+
+    delete 'habits/:id' do 
+
     end
 end
